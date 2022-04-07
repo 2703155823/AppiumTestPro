e4e41c62a315fc89d4f00a321f95640b64e4012d
当权 欧 <2703155823@qq.com>
2022-04-07 22:45:18 +0800

    init

--- customLib/writeLog.py
+++ customLib/writeLog.py
@@ -3,26 +3,28 @@ import os
 import time
 
 
-def Create_Log(dir_name, name, msg):
+def Create_Log(dir_name , name , msg) :
     desktop_path = './' + dir_name + '/'
     # 判断文件夹是否存在，不存在则创建
-    if not os.path.exists(desktop_path):
-        os.mkdir(desktop_path)
+    if not os.path.exists ( desktop_path ) :
+        os.mkdir ( desktop_path )
     # 判断文件是否被创建，不存在则创建;创建了，就追加内容
     file_path = desktop_path + name + '.log'
-    if not os.path.exists(file_path):
-        file = open(file_path, 'w+')
-        timer = time.strftime("%Y年%m月%d日 %H时%M分%S秒", time.localtime())
+    if not os.path.exists ( file_path ) :
+        file = open ( file_path , 'w+' )
+        timer = time.strftime ( "%Y年%m月%d日 %H时%M分%S秒" , time.localtime () )
         msg = timer + ' *** ' + msg
-    else:
-        file = open(file_path, 'a+')
-        timer = time.strftime("%Y年%m月%d日 %H时%M分%S秒", time.localtime())
+    else :
+        file = open ( file_path , 'a+' )
+        timer = time.strftime ( "%Y年%m月%d日 %H时%M分%S秒" , time.localtime () )
         msg = '\n' + timer + ' *** ' + msg
-    file.write(msg)
-    file.close()
+    file.write ( msg )
+    file.close ()
 
-def write_Runlog(message):
-    Create_Log('log', 'Runlog', message)  # 调用函数
 
-def write_Errlog(message):
-    Create_Log('log', 'Errlog', message)  # 调用函数
+def write_Runlog(message) :
+    Create_Log ( 'log' , 'Runlog' , message )  # 调用函数
+
+
+def write_Errlog(message) :
+    Create_Log ( 'log' , 'Errlog' , message )  # 调用函数
